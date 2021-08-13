536a87d4a416019e0dda49d9f252f44ec50a3864
Juan <juand8012@gmail.com>
2021-08-13 11:08:00 -0500

    Fetching API was created, but it doesnt work

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,25 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import styles from "../styles/Home.module.scss";
+import Image from 'next/image'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
     </>
   );
 }
-export default MyApp;
+export default App;
